383da6f3d1f5699414bcdff682d4ea44f062a678
Neal Milstein <neal.milstein@gmail.com>
2020-10-09 02:26:47 +0200

    Move CreatedAt column to end of all models

--- app/models/conversation.go
+++ app/models/conversation.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Conversation struct {
-	ID        int       `gorm:"primaryKey,not null"`
-	CreatedAt time.Time `gorm:"not null"`
-	Users     []User    `gorm:"many2many:conversation_users;"`
+	ID        int    `gorm:"primaryKey,not null"`
+	Users     []User `gorm:"many2many:conversation_users;"`
 	Messages  []Message
+	CreatedAt time.Time `gorm:"not null"`
 }
--- app/models/message.go
+++ app/models/message.go
@@ -8,6 +8,6 @@ type Message struct {
 	ID             int       `gorm:"primaryKey,not null"`
 	UserID         int       `gorm:"not null"`
 	ConversationID int       `gorm:"not null"`
-	CreatedAt      time.Time `gorm:"not null"`
 	Body           string    `gorm:"not null"`
+	CreatedAt      time.Time `gorm:"not null"`
 }
