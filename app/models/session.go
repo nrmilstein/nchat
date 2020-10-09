c5747ec17356b3f815cfacc35314a7368daefc2c
Neal Milstein <neal.milstein@gmail.com>
2020-10-09 02:25:01 +0200

    Fix typo in session.go Createdt -> CreatedAt

--- app/models/session.go
+++ app/models/session.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Session struct {
-	ID       int    `gorm:"primaryKey"`
-	Key      string `gorm:"not null"`
-	User     User
-	UserID   int       `gorm:"not null"`
-	Createdt time.Time `gorm:"not null"`
+	ID        int    `gorm:"primaryKey"`
+	Key       string `gorm:"not null"`
+	User      User
+	UserID    int       `gorm:"not null"`
+	CreatedAt time.Time `gorm:"not null"`
 	// AccessedAt time.Time
 }
