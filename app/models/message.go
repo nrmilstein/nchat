f453b315b8bfabcaa7dcf9b2a7c31b30e19280ad
Neal Milstein <neal.milstein@gmail.com>
2020-10-20 23:15:02 +0200

    Fix broken query to get conversation between two users.

--- app/models/message.go
+++ app/models/message.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"errors"
 	"time"
 
 	"github.com/nrmilstein/nchat/db"
@@ -13,6 +14,9 @@ type Message struct {
 	Body           string    `gorm:"not null"`
 	CreatedAt      time.Time `gorm:"not null"`
 }
+
+var ErrConversationNotFound = errors.New("Conversation not found.")
+var ErrTooManyConversations = errors.New("Too many conversations found between given users.")
 
 type GormError struct {
 	err error
@@ -37,11 +41,9 @@ func newGormError(e error) GormError {
 func CreateMessage(sender *User, recipient *User, body string) (*Message, error) {
 	db := db.GetDb()
 
-	var senderConversations []Conversation
-	err := db.Model(&sender).Preload("Users", "ID = ?", recipient.ID).
-		Association("Conversations").Find(&senderConversations)
-	if err != nil {
-		return nil, newGormError(err)
+	conversation, err := GetConversation(sender, recipient)
+	if !errors.Is(err, ErrConversationNotFound) && err != nil {
+		return nil, err
 	}
 
 	newMessage := Message{
@@ -49,7 +51,7 @@ func CreateMessage(sender *User, recipient *User, body string) (*Message, error)
 		Body:   body,
 	}
 
-	if len(senderConversations) == 0 {
+	if errors.Is(err, ErrConversationNotFound) {
 		newConversation := &Conversation{
 			Users: []User{
 				*sender,
@@ -64,11 +66,29 @@ func CreateMessage(sender *User, recipient *User, body string) (*Message, error)
 			return nil, newGormError(result.Error)
 		}
 	} else {
-		conversation := senderConversations[0]
 		err := db.Model(&conversation).Association("Messages").Append(&newMessage)
 		if err != nil {
 			return nil, newGormError(err)
 		}
 	}
 	return &newMessage, nil
+}
+
+func GetConversation(sender *User, recipient *User) (*Conversation, error) {
+	db := db.GetDb()
+
+	var senderConversations []Conversation
+	err := db.Model(&sender).Preload("Users", "ID = ?", recipient.ID).
+		Association("Conversations").Find(&senderConversations)
+	if err != nil {
+		return nil, newGormError(err)
+	}
+
+	for _, conversation := range senderConversations {
+		if len(conversation.Users) == 1 {
+			return &conversation, nil
+		}
+	}
+
+	return nil, ErrConversationNotFound
 }
