7f011ac2b7ff1c151107779c370868c5443bbf45
Neal Milstein <neal.milstein@gmail.com>
2020-10-26 00:27:49 +0100

    Add and serve nchat-web client submodule

--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ module github.com/nrmilstein/nchat
 go 1.14
 
 require (
+	github.com/gin-contrib/static v0.0.0-20200916080430-d45d9a37d28e
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/heroku/x v0.0.24
 	github.com/lib/pq v1.7.0
-	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gorm.io/driver/postgres v1.0.2
 	gorm.io/gorm v1.20.2
 	nhooyr.io/websocket v1.8.6
--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 
+	"github.com/gin-contrib/static"
 	"github.com/nrmilstein/nchat/app/controllers"
 	"github.com/nrmilstein/nchat/app/controllers/chatServer"
 	"github.com/nrmilstein/nchat/app/middlewares"
@@ -47,14 +48,13 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
-	router.Use(middlewares.JSONContentType())
-	router.Use(middlewares.ErrorHandler())
-	//router.Static("/static", "static")
 
 	hub := chatServer.NewHub()
 
 	api := router.Group("/api/v1")
 	{
+		api.Use(middlewares.JSONContentType())
+		api.Use(middlewares.ErrorHandler())
 		api.POST("/users", controllers.PostUsers)
 		api.GET("/users/:email", controllers.GetUser)
 		api.POST("/authenticate", controllers.PostAuthenticate)
@@ -63,6 +63,8 @@ func main() {
 		api.GET("/conversations/:id", controllers.GetConversation)
 		api.GET("/chat", hub.GetChat)
 	}
+
+	router.Use(static.Serve("/", static.LocalFile("./nchat-web/build", true)))
 
 	router.NoRoute(controllers.NoRoute)
 
