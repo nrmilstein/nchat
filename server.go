0cc829d74636ca8fa1f18511981317347a455fa5
Neal Milstein <neal.milstein@gmail.com>
2020-11-05 23:21:32 +0100

    Add routes for /accounts/*

--- server.go
+++ server.go
@@ -66,6 +66,8 @@ func main() {
 	}
 
 	router.Use(static.Serve("/", static.LocalFile("./nchat-web", true)))
+	router.Use(static.Serve("/accounts/login", static.LocalFile("./nchat-web", true)))
+	router.Use(static.Serve("/accounts/signup", static.LocalFile("./nchat-web", true)))
 
 	router.NoRoute(controllers.NoRoute)
 
