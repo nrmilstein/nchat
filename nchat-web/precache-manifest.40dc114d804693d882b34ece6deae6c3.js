bf1b91580a70585f21e8536c4d25d6d392284bc1
Neal Milstein <neal.milstein@gmail.com>
2020-11-06 20:19:18 +0100

    Update nchat-web client to latest version.
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.
    
    Update nchat-web client to latest version.

---
+++ nchat-web/precache-manifest.40dc114d804693d882b34ece6deae6c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bddf773222efd12eeff39b8b776b4ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1346a226c22b89ab5f6",
+    "url": "/static/css/main.6cb510e4.chunk.css"
+  },
+  {
+    "revision": "49cab599a2bb07115b0f",
+    "url": "/static/js/2.a026b93f.chunk.js"
+  },
+  {
+    "revision": "b1e1af3d04fcf4054bede355b63cfc48",
+    "url": "/static/js/2.a026b93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1346a226c22b89ab5f6",
+    "url": "/static/js/main.612f1487.chunk.js"
+  },
+  {
+    "revision": "a01960faba65615d1612",
+    "url": "/static/js/runtime-main.7a2879b3.js"
+  },
+  {
+    "revision": "9955ebcfc906dc0b2b0614d8e4a06f8f",
+    "url": "/static/media/greenCheck.9955ebcf.svg"
+  },
+  {
+    "revision": "a4eca1ad794b742201d38541b089a0ee",
+    "url": "/static/media/loading.a4eca1ad.svg"
+  },
+  {
+    "revision": "09a696786cdbc74f4e92228a34cc2749",
+    "url": "/static/media/logo.09a69678.svg"
+  },
+  {
+    "revision": "70d85da35f081e040b320b161d526760",
+    "url": "/static/media/logoCompact.70d85da3.svg"
+  },
+  {
+    "revision": "e43442ba15a7167dbc325e6f7f447e0c",
+    "url": "/static/media/logout.e43442ba.svg"
+  },
+  {
+    "revision": "8461f02d21cb88358901d88a62207112",
+    "url": "/static/media/plus.8461f02d.svg"
+  },
+  {
+    "revision": "9d8ae0c25baddd1f16b9cae3ec6a4310",
+    "url": "/static/media/redX.9d8ae0c2.svg"
+  }
+]);
